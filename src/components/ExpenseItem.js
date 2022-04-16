caee4f6a54d9ed905a82f11b9a57dfca127630d4
Mateusz Barwicki <mateuszbarwicki2@gmail.com>
2022-04-16 12:15:33 +0200

    Add props to ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
   return (
     <div className="expense-item">
-      <div>March 28th 2022</div>
+      <div>{props.date}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$350.47</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
