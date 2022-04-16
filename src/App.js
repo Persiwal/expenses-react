78333c55dd23e47da680bc7abe1fe633c98aa0b0
Mateusz Barwicki <mateuszbarwicki2@gmail.com>
2022-04-16 15:32:44 +0200

    Create Expenses component and add styles to it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
  const expenses = [
@@ -26,7 +26,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!s</h2>
-      
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
